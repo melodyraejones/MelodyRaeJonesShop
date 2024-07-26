1e7641f401323779a3f6ffaf82d7f62f68abf0e3
Akshay sharma <akshaysharma5432@gmail.com>
2024-07-25 22:16:42 -0400

    fix contact message sent page text

--- page-sent.php
+++ page-sent.php
@@ -8,7 +8,7 @@ get_header();
 <div class="cta-image-box-thankyou" role="img" aria-label="Woman meditating"></div>
 <h2 class="thankyou-message">Thank you for your message.</h2>
 <p class="thankyou-info">We will contact you soon.</p>
-<p class="program-navigation">In the meantime,  explore my <a href="https://melodyraejones.com/offerings/main.html"</a> and find the one that best suits your needs.</p>
+<p class="program-navigation">In the meantime,  explore my offerings<a href="https://melodyraejones.com/offerings/main.html"</a> and find the one that best suits your needs.</p>
 
 </section>
 <?
